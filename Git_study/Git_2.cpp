b0a383199f4065d5b9814671072d685fa4de6d7f
Songsungho <songsungho1@naver.com>
2018-05-25 20:11:00 +0900

    Implement sub function

--- Git_study/Git_2.cpp
+++ Git_study/Git_2.cpp
@@ -7,13 +7,15 @@ int main() {
 	char select;
 	cout << "두 수를 입력하세요 : ";
 	cin >> a >> b;
-	cout << endl << "사칙연산의 부호를 입력하세요 : ";
+	cout << "사칙연산의 부호를 입력하세요 : ";
 	cin >> select;
-	cout << endl;
 	switch (select)
 	{
 	case '+':
 		cout << a + b << endl;
+		break;
+	case '-':
+		cout << a - b << endl;
 		break;
 	default:
 		break;
